e108b1fb9541e0c80028047da1993638495b3c3e
mutianyu <283380371@qq.com>
2016-01-23 17:25:13 +0800

    我搭建的框架

---
+++ shili/shili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shili
+//
+//  Created by xalo on 16/1/23.
+//  Copyright © 2016年 穆森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
